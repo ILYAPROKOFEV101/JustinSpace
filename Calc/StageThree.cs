52ddbc4a0e283e61fabf02a405b19dcec5b7dc9a
Alex_Larin <sashalarin55555@gmail.com>
2025-06-06 18:06:22 +0300

    Add calculate module

---
+++ Calc/StageThree.cs
@@ -0,0 +1,57 @@
+﻿namespace Calc;
+
+public class StageThree : Stages
+{
+    private readonly StageTwo stageTwo;
+
+    public StageThree(StageTwo stageTwo, double dryMass, double fuelMass, double fuelConsumptionRate)
+    {
+        this.stageTwo = stageTwo;
+
+        this.dryMass = dryMass;
+        this.fuelMass = fuelMass;
+        this.fuelConsumptionRate = fuelConsumptionRate;
+
+        t = 224.0;
+        F = 294000.0;
+        angle = 8.0;
+
+        CalculateTimeValues();
+        CalculateFunction();
+    }
+
+    public void CalculateFunction()
+    {
+        for (int i = 0; i < timeValues.Count; i++)
+        {
+            double time = timeValues[i];
+
+            accelerationXValues.Add(XFunction(time));
+            accelerationYValues.Add(YFunction(time));
+
+            speedXValues.Add(Euler(SetStartValues(speedXValues, stageTwo.SpeedXValues, i), accelerationXValues[i]));
+            speedYValues.Add(Euler(SetStartValues(speedYValues, stageTwo.SpeedYValues, i), accelerationYValues[i]));
+
+            movementXValues.Add(Euler(SetStartValues(movementXValues, stageTwo.MovementXValues, i), speedXValues[i]));
+            movementYValues.Add(Euler(SetStartValues(movementYValues, stageTwo.MovementYValues, i), speedYValues[i]));
+        }
+
+        PrintParameters();
+    }
+
+    private double XFunction(double arg)
+    {
+        double baseAngle = stageTwo.stageOne.GetEndAngle() + stageTwo.GetEndAngle();
+        double totalAngle = baseAngle + RotationAngleFunction() * arg;
+        double mass = GetCurrentMass(arg);
+        return (F * Math.Sin(totalAngle)) / mass;
+    }
+
+    private double YFunction(double arg)
+    {
+        double baseAngle = stageTwo.stageOne.GetEndAngle() + stageTwo.GetEndAngle();
+        double totalAngle = baseAngle + RotationAngleFunction() * arg;
+        double mass = GetCurrentMass(arg);
+        return (F * Math.Cos(totalAngle)) / mass - g;
+    }
+}
---
+++ Calc/StageTwo.cs
@@ -0,0 +1,55 @@
+﻿namespace Calc;
+
+public class StageTwo : Stages
+{
+    public readonly StageOne stageOne;
+
+    public StageTwo(StageOne stageOne, double dryMass, double fuelMass, double fuelConsumptionRate)
+    {
+        this.stageOne = stageOne;
+
+        this.dryMass = dryMass;
+        this.fuelMass = fuelMass;
+        this.fuelConsumptionRate = fuelConsumptionRate;
+
+        t = 182.0;
+        F = 921200.0;
+        angle = 16.0;
+
+        CalculateTimeValues();
+        CalculateFunction();
+    }
+
+    public void CalculateFunction()
+    {
+        for (int i = 0; i < timeValues.Count; i++)
+        {
+            double time = timeValues[i];
+
+            accelerationXValues.Add(XFunction(time));
+            accelerationYValues.Add(YFunction(time));
+
+            speedXValues.Add(Euler(SetStartValues(speedXValues, stageOne.SpeedXValues, i), accelerationXValues[i]));
+            speedYValues.Add(Euler(SetStartValues(speedYValues, stageOne.SpeedYValues, i), accelerationYValues[i]));
+
+            movementXValues.Add(Euler(SetStartValues(movementXValues, stageOne.MovementXValues, i), speedXValues[i]));
+            movementYValues.Add(Euler(SetStartValues(movementYValues, stageOne.MovementYValues, i), speedYValues[i]));
+        }
+
+        PrintParameters();
+    }
+
+    private double XFunction(double arg)
+    {
+        double totalAngle = stageOne.GetEndAngle() + RotationAngleFunction() * arg;
+        double mass = GetCurrentMass(arg);
+        return (F * Math.Sin(totalAngle)) / mass;
+    }
+
+    private double YFunction(double arg)
+    {
+        double totalAngle = stageOne.GetEndAngle() + RotationAngleFunction() * arg;
+        double mass = GetCurrentMass(arg);
+        return (F * Math.Cos(totalAngle)) / mass - g;
+    }
+}
