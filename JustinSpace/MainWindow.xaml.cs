6a47c204b2955d235411b5dc7e14dd9897ed720e
Ilya <ilaprokofev14@gmail.com>
2025-06-09 12:42:05 +0300

    new ui

--- JustinSpace/MainWindow.xaml.cs
+++ JustinSpace/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ namespace JustinSpace
         private Calculator currentCalculator;
         private int animationStepIndex = 0;
         private DispatcherTimer animationTimer;
+        private double zoomFactor = 1.0;
+        private readonly double minZoom = 0.5;
+        private readonly double maxZoom = 3.0;
+        
 
         public MainWindow()
         {
@@ -73,6 +77,30 @@ namespace JustinSpace
             rocketTranslate.Y = y;
 
             animationStepIndex++;
+        }
+        
+        private void ZoomInButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (zoomFactor < maxZoom)
+            {
+                zoomFactor = Math.Min(zoomFactor + 0.1, maxZoom);
+                ApplyZoom();
+            }
+        }
+
+        private void ZoomOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (zoomFactor > minZoom)
+            {
+                zoomFactor = Math.Max(zoomFactor - 0.1, minZoom);
+                ApplyZoom();
+            }
+        }
+
+        private void ApplyZoom()
+        {
+            SceneCanvasScaleTransform.ScaleX = zoomFactor;
+            SceneCanvasScaleTransform.ScaleY = zoomFactor;
         }
     }
 }
