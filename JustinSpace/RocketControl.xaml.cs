3ad905498fc15c71d7710d44b63afce4de486beb
Ivan <kazak1221209@gmail.com>
2025-06-17 19:55:06 +0300

    добавил новую ракету

---
+++ JustinSpace/RocketControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls; 
+using System.Windows;         
+using System.Windows.Media;    
+
+namespace JustinSpace.Controls
+{
+    public partial class RocketControl : UserControl
+    {
+        public RocketControl()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowFlame(bool visible)
+        {
+            RocketFlame.Visibility = visible ? Visibility.Visible : Visibility.Hidden;
+        }
+    }
+}
