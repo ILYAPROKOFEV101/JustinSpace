e9f9e044f804a756e1e22e10d7ad847c905decfe
Ivan <kazak1221209@gmail.com>
2025-06-19 11:16:52 +0300

    выбор

--- JustinSpace/RocketControl.xaml.cs
+++ JustinSpace/RocketControl.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Windows.Controls; 
-using System.Windows;         
-using System.Windows.Media;    
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace JustinSpace.Controls
 {
@@ -9,8 +9,43 @@ namespace JustinSpace.Controls
         public RocketControl()
         {
             InitializeComponent();
+            SetRocket("rocket_photo"); // дефолтная ракета
         }
 
-        
+        public void SetRocket(string rocketName)
+        {
+            string imagePath = null;
+
+            switch (rocketName)
+            {
+                case "rocket_photo":
+                    imagePath = "pack://application:,,,/Assets/rocket_photo.png";
+                    break;
+                case "rocket1":
+                    imagePath = "pack://application:,,,/Assets/photo1.png";
+                    break;
+                case "rocket2":
+                    imagePath = "pack://application:,,,/Assets/photo2.png";
+                    break;
+                case "rocket3":
+                    imagePath = "pack://application:,,,/Assets/photo3.png";
+                    break;
+            }
+
+            if (imagePath == null)
+            {
+                RocketImage.Source = null;
+                return;
+            }
+
+            try
+            {
+                RocketImage.Source = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+            }
+            catch
+            {
+                RocketImage.Source = null;
+            }
+        }
     }
 }
---
+++ JustinSpace/RocketSelectorControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace JustinSpace.Controls
+{
+    public partial class RocketSelectorControl : UserControl
+    {
+        public delegate void RocketSelectedEventHandler(string rocketType);
+        public event RocketSelectedEventHandler RocketSelected;
+    
+        public RocketSelectorControl()
+        {
+            InitializeComponent();
+
+            var rockets = new List<RocketItem>()
+            {
+                new RocketItem() { Name = "Ракета 1", ImagePath = "/assets/photo1.png" },
+                new RocketItem() { Name = "Ракета 2", ImagePath = "/assets/photo2.png" },
+                new RocketItem() { Name = "Ракета 3", ImagePath = "/assets/photo3.png" }
+            };
+
+            
+        }
+
+        private void RocketComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (RocketComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string rocketTag = selectedItem.Tag?.ToString();
+                RocketSelected?.Invoke(rocketTag);
+            }
+        }
+        public void SetRocket(string rocketName)
+        {
+            switch(rocketName)
+            {
+                case "rocket1":
+                    // Логика смены изображения, цвета, видимых частей
+                    break;
+                case "rocket2":
+                    // ...
+                    break;
+                case "rocket3":
+                    // ...
+                    break;
+            }
+        }
+
+
+    }
+    
+
+    public class RocketItem
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
